dff8f2f652e1ee4ad0d6e329a31840611a051065
Michael SN <michael.dev.nascimento@gmail.com>
2022-08-22 10:00:48 -0300

    comp[ProductList]: component ProductList mount, but not finished

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-
+  <div class="products-container">
+    <div v-for="product in products" :key="product.id">
+    <img v-if="product.photos"  :src="product.photos[0].src" :alt="product.photos[0].title">
+    <h2>{{product.name}}</h2>
+    <p>{{product.price}}</p>
+    <p>{{product.description}}</p>
+    </div>      
   </div>
 </template>
 
-<script>
+<script>  
   export default {
-    name : "ProductsList"
+    name : "ProductsList",
+    data() {
+      return {
+        products: null
+      }
+    },
+    methods: {
+      getProducts() {
+        fetch("http://localhost:3000/product")
+        .then(res => res.json())
+        .then(respJson => {          
+          this.products = respJson
+        })
+        .catch((e)=> {
+          console.error('There has been a problem with your fetch operation: ' + e.message);
+        })
+      }
+    },    
+    created() {
+      this.getProducts()
+    }
   }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
