3f860a3cb2b1c639b1b73b27939d9e1bfe2eb393
Michael SN <michael.dev.nascimento@gmail.com>
2022-08-26 17:31:40 -0300

    feat[User]: delete product

--- src/views/users/UserProduct.vue
+++ src/views/users/UserProduct.vue
@@ -7,6 +7,7 @@
       <li v-for="(product, index) in user_products" :key="index">
         <ProductItem :product="product">
           <p>{{ product.description }}</p>
+          <button @click="deleteProduct(product.id)" class="btn-delete">Deletar Produto</button>
         </ProductItem>
       </li>
     </transition-group>
@@ -18,6 +19,7 @@ import { mapState, mapActions } from 'vuex';
 
 import AddProduct from '@/components/AddProduct.vue';
 import ProductItem from '@/components/ProductItem.vue'
+import { api } from '@/axios';
 
 export default {
   name: "UserProduct",
@@ -29,7 +31,17 @@ export default {
     ...mapState(["login", "user", "user_products"])
   },
   methods: {
-    ...mapActions(["getUserProducts"])
+    ...mapActions(["getUserProducts"]),
+    deleteProduct(id) {
+      const confirm = window.confirm("Deseja remover este item")
+      if (confirm) {
+        api.delete(`/product/${id}`).then(() => {
+          this.getUserProducts()
+        }).catch(error => {
+          console.log(error)
+        })
+      }
+    }
   },
   watch: {
     login() {
@@ -58,5 +70,18 @@ h2 {
 .list-enter-active,
 .list-leave-active {
   transition: all .3s;
+}
+
+.btn-delete {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 24px;
+  height: 24px;
+  text-indent: -140px;
+  overflow: hidden;
+  cursor: pointer;
+  background: url('../../assets/remove.svg') no-repeat center center;
+  border: none;
 }
 </style>
