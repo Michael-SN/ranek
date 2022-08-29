4322598635429b21a9ec1b0cd707660198cc3b57
Michael SN <michael.dev.nascimento@gmail.com>
2022-08-29 18:19:08 -0300

    feat[UserBuy]: component in progress

--- src/views/users/UserBuy.vue
+++ src/views/users/UserBuy.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>
-    <p>Lista de Compras</p>
-  </div>
+  <section>
+    <div v-if="purchases">
+      <h2>Compras</h2>
+      <div class="products-wrapper" v-for="(purchase, index) in purchases" :key="index">
+        <ProductItem v-if="purchase.product" :product="purchase.product">
+          <p class="seller"><span>Vendedor</span>{{ purchase.seller_id }}</p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { api } from '@/axios';
+import ProductItem from '@/components/ProductItem.vue';
+import { mapState } from 'vuex';
 export default {
-  name: "UserBuy"
+  name: "UserBuy",
+  data() {
+    return {
+      purchases: null,
+    }
+  },
+  components: {
+    ProductItem,
+  },
+  computed: {
+    ...mapState(["user", "login"])
+  },
+  methods: {
+    getPurchases() {
+      api.get(`transaction?buyer_id=${this.user.id}`).then(res => {
+        this.purchases = res.data
+      })
+    }
+  },
+  watch: {
+    login() {
+      this.getPurchases()
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getPurchases()
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.product-wrapper {
+  margin-bottom: rem(40);
+
+
+  .seller {
+    span {
+      margin-bottom: rem(20);
+    }
+  }
+}
 </style>
